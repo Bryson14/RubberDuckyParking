3843b144290085646fd471375c9cd33ca9a4a91c
Bryson <bryson.meiling@aggiemail.usu.edu>
2021-02-09 09:45:08 -0700

    added skeleton for the urls and views

--- src/RubberDuckyParking/parking/urls.py
+++ src/RubberDuckyParking/parking/urls.py
@@ -4,8 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('host', views.host_index, name='host'),
+    path('host/<str:host_id>/', views.host_detail, name='host_detail'),
+    path('host/', views.host_index, name='host'),
+    path('tools/', views.lot_tools, name='lot_tools'),
+    path('attendant/<str:attendant_id>/', views.attendant_detail, name="attendant_details"),
+    path('reservation/<str:res_id>/', views.reservation, name="reservation"),
+    path('user/<str:user_id>/', views.user_detail, name='user_detail'),
     path('images/hosts/', views.get_image_host, name='image_host'),
     path('images/users/', views.get_image_user, name='image_user'),
-    path('images/locations/', views.get_image_loc, name='image_loc')
+    path('images/locations/', views.get_image_loc, name='image_loc'),
+    path('login', views.login, name="login")
 ]
--- src/RubberDuckyParking/parking/views.py
+++ src/RubberDuckyParking/parking/views.py
@@ -2,24 +2,60 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from os import pathsep
 
+
 def index(request):
     context = {}
     return render(request, 'parking/index.html', context)
 
+
 def host_index(request):
     context = {}
     return render(request, 'parking/host_index.html', context)
+
+
+def host_detail(request, host_id="empty"):
+    context = {"host_id": host_id}
+    return render(request, 'parking/host_details.html', context)
+
+
+def user_detail(requst, user_id="Empty"):
+    context = {"user_id": user_id}
+    return render(requst, 'parking/user_details.html', context)
+
+
+def lot_tools(request):
+    context = {}
+    return render(request, 'parking/parking_lot_tools.html', context)
+
+
+def attendant_detail(request, attendant_id="Empty"):
+    context = {"attendant_id": attendant_id}
+    return render(request, 'parking/attendant_details.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'parking/login.html', context)
+
+
+def reservation(request, res_id="None"):
+    context = {"reservation_id": res_id}
+    return render(request, 'parking/reservation_detail.html', context)
+
 
 def get_image(filename, folder):
     p = f'..{pathsep}{folder}{pathsep}{filename}'
     print(p)
     return(open(p))
 
+
 def get_image_host(request):
     return HttpResponse(get_image(request.path, 'host'), content_type='image\jpeg')
 
+
 def get_image_user(request):
     return HttpResponse(get_image(request.path, 'users'), content_type='image\jpeg')
+
 
 def get_image_loc(request):
     return HttpResponse(get_image(request.path, 'location'), content_type='image\jpeg')
