1e0ead3f08542368311c126d4225300a8f1dd48e
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2021-02-18 08:57:38 -0700

    start creating signal

--- src/RubberDuckyParking/parking_api/signals.py
+++ src/RubberDuckyParking/parking_api/signals.py
@@ -1,8 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import BaseUser
+import os, random
 
 @receiver(post_save, sender=BaseUser, dispatch_uid="assign_profile_pic")
-def assign_profile_pic(sender, instance, **kwargs):
+def assign_profile_pic(sender, instance, created, **kwargs):
     ## this is where we will assign the random profile picture
-    pass
+    print(os.listdir("static/default_avatars"))
+    if created:
+        instance.avatar = random.choice(os.listdir("static/default_avatars"))
+        instance.save()
